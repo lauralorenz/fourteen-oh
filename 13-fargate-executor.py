e793e226a575679067f3f19b01625e29628870a6
Prefect <hooloovoo@Lauras-MBP.fios-router.home>
2020-12-18 09:21:34 -0500

    back to S3 for 13, and add the dask examples

--- 13-fargate-executor.py
+++ 13-fargate-executor.py
@@ -1,5 +1,6 @@
 from prefect.environments.storage import S3
 from prefect.environments import FargateTaskEnvironment
+from prefect.engine.executors import DaskExecutor, LocalDaskExecutor
 from prefect import task, Flow, Parameter
 import prefect
 
@@ -7,15 +8,17 @@ STORAGE = S3(bucket="demo-prefect-flows-14")
 
 ENVIRONMENT = FargateTaskEnvironment(
 		networkMode="awsvpc",
-		family="13-fargate", 
-		taskDefinition="13-fargate",
+		family="13-fargate-dask", 
+		taskDefinition="13-fargate-dask",
 		cpu="256",  
 		memory="512", 
 		cluster="prefect-demo-cluster",
 	 	networkConfiguration={'awsvpcConfiguration': {'assignPublicIp': 'ENABLED', 'subnets': ['subnet-7410175c'], 'securityGroups': []}},
 	 	taskRoleArn="arn:aws:iam::136638793011:role/prefect-demo-fargate-task-role",
  		executionRoleArn="arn:aws:iam::136638793011:role/ecsTaskExecutionRole",
-	  	metadata={"image":'prefecthq/prefect:all_extras-0.13.19'})
+	  	metadata={"image":'prefecthq/prefect:all_extras-0.13.19'},
+	  	executor=DaskExecutor(cluster_class="dask_cloudprovider.aws.FargateCluster",
+    			cluster_kwargs={"n_workers": 4, "image": "prefecthq/prefect:all_extras-0.13.19"}))
 
 @task
 def hi():
@@ -23,8 +26,21 @@ def hi():
    logger.info("Hello!")
 
 
-with Flow("prefect-13", storage=STORAGE, environment=ENVIRONMENT) as flow:
+with Flow("prefect-13-dask", storage=STORAGE, environment=ENVIRONMENT) as flow:
 	hi()
+
+# if __name__ == '__main__':
+# 	import logging
+# 	import sys
+# 	logger = logging.getLogger('distributed.scheduler')
+# 	logger.setLevel('DEBUG')
+# 	log_stream = logging.StreamHandler(sys.stdout)
+# 	logger.addHandler(log_stream)
+
+
+# 	flow.run()
+	
+	#flow.run(executor=DaskExecutor(cluster_kwargs={"n_workers": 4, "threads_per_worker": 2}))
 
 flow.register("14-demo")
 
---
+++ 14-fargate-executor.py
@@ -0,0 +1,54 @@
+from prefect.storage import GitHub
+from prefect.run_configs import ECSRun
+from prefect.executors import DaskExecutor
+from prefect import task, Flow, Parameter
+import prefect
+
+STORAGE = GitHub(repo="lauralorenz/fourteen-oh", ref="main", path="/14-fargate-executor.py")
+
+RUN_CONFIG = ECSRun(
+	run_task_kwargs={
+		#"networkMode":"awsvpc",
+		#"taskDefinition":"14-fargate",
+		"cluster":"prefect-demo-cluster",
+	 	"networkConfiguration": {'awsvpcConfiguration': {'assignPublicIp': 'ENABLED', 'subnets': ['subnet-7410175c'], 'securityGroups': []}},
+ 		#"executionRoleArn": "arn:aws:iam::136638793011:role/ecsTaskExecutionRole"
+ 		},
+	task_role_arn="arn:aws:iam::136638793011:role/prefect-demo-fargate-task-role",
+	#image='prefecthq/prefect:all_extras-0.13.19',
+	memory="512", 
+	cpu="256")
+
+EXECUTOR = DaskExecutor(
+    cluster_class="dask_cloudprovider.aws.FargateCluster",
+    cluster_kwargs={"n_workers": 4, "image": 'prefecthq/prefect:all_extras-0.14.0'})
+
+
+@task
+def hi():
+   logger = prefect.context.get('logger')
+   logger.info("Hello!")
+
+
+with Flow("prefect-14-dask", storage=STORAGE, run_config=RUN_CONFIG, executor=EXECUTOR) as flow:
+	hi()
+
+flow.register("14-demo")
+
+#########
+# agent #
+#########
+
+if __name__ == '__main__':
+	from prefect.agent.ecs.agent import ECSAgent
+
+	AGENT = ECSAgent(#labels=["s3-flow-storage"],
+		#cpu="256", 
+		#memory="512",
+		cluster="prefect-demo-cluster", 
+		#networkConfiguration={'awsvpcConfiguration': {'assignPublicIp': 'ENABLED', 'subnets': ['subnet-7410175c'], 'securityGroups': []}}, 
+		task_role_arn="arn:aws:iam::136638793011:role/prefect-demo-fargate-task-role",
+	 	#executionRoleArn="arn:aws:iam::136638793011:role/ecsTaskExecutionRole"
+	 	)
+
+	AGENT.start()
